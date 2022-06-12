d70e51799b602e046aa20d33ea7084da41542222
keiinuki <k.inuki@haguruma.co.jp>
2022-06-12 14:21:53 +0900

    フォーマット修正

--- src/main/java/com/inuki/HelloWorld/HelloWorldController.java
+++ src/main/java/com/inuki/HelloWorld/HelloWorldController.java
@@ -1,14 +1,13 @@
 package com.inuki.HelloWorld;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController{	
-	
+public class HelloWorldController {
+
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello world";
-	}	
+	}
 }
