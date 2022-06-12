006c2698e6572d99ad5228704709dd25bc4e0096
keiinuki <k.inuki@haguruma.co.jp>
2022-06-12 14:45:10 +0900

    GreetingControllerを追加

---
+++ src/main/java/com/inuki/HelloWorld/Greeting.java
@@ -0,0 +1,20 @@
+package com.inuki.HelloWorld;
+
+public class Greeting {
+
+	private final Integer id;
+	private final String content;
+
+	public Greeting(Integer id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
