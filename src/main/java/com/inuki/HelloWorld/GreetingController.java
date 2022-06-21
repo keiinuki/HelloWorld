b259b91fd6c8b7e5306002d9cd9af71acf6532be
keiinuki <k.inuki@haguruma.co.jp>
2022-06-22 06:34:47 +0900

    idのインクリメントをAtomicを使わない形式に修正

--- src/main/java/com/inuki/HelloWorld/GreetingController.java
+++ src/main/java/com/inuki/HelloWorld/GreetingController.java
@@ -1,7 +1,5 @@
 package com.inuki.HelloWorld;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     private final String template = "Hello, %s!";
-    private final AtomicInteger counter = new AtomicInteger();
+    private Integer counter = 0;
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-	return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	return new Greeting(counter++, String.format(template, name));
     }
 }
